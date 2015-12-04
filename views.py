9c5248cb1c8a965c3b847da62fd31b1ca20b989c
Jon Barclay <jonbarclay@gmail.com>
2015-12-04 13:03:45 -0700

    update models remove old saq_req renamed saq_requirement

--- views.py
+++ views.py
@@ -37,8 +37,9 @@ class RequirementList(ListView):
 @login_required(login_url='/login/')   
 def merchant_requirement(request, merchant_name, version, saq_req, req_number):
     # if this is a POST request we need to process the form data
+    saq = get_object_or_404(SAQ, saq_name=saq_req, saq_version=version)
     merchant = get_object_or_404(Merchant, merchant_name=merchant_name)
-    req = get_object_or_404(Requirement, req_number=req_number, version=version, saq_req=saq_req)
+    req = get_object_or_404(Requirement, req_number=req_number, saq_req=saq)
     merch_requirement = get_object_or_404(Merch_Requirement, requirement=req, merchant=merchant)
     req_children = req.child_req.all()
     merch_req_children = Merch_Requirement.objects.filter(requirement__in=req_children).filter(merchant=merchant).order_by(
