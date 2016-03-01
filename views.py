bd202033c9fafe13bb341475f50afedcdfd50c47
Jon Barclay <jon.barclay@gmail.com>
2016-03-01 09:56:59 -0700

    updated models and templates

--- views.py
+++ views.py
@@ -12,7 +12,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Requirement, Testing_Procedure, Merchant, Finding, Merch_Requirement, SAQ
 from .forms import NameForm
 
-users_in_group = Group.objects.get(name="group name").user_set.all()
+
 
 
 @login_required(login_url='/login/')
@@ -39,6 +39,7 @@ class RequirementList(ListView):
     
 @login_required(login_url='/login/')   
 def merchant_requirement(request, merchant_name_URL, version, saq_req, req_number):
+    users_in_group = request.user.groups.filter(name='pcidocs-admins').exists()
     # if this is a POST request we need to process the form data
     saq = get_object_or_404(SAQ, saq_name=saq_req, saq_version=version)
     merchant = get_object_or_404(Merchant, merchant_name_URL=merchant_name_URL)
@@ -48,8 +49,9 @@ def merchant_requirement(request, merchant_name_URL, version, saq_req, req_numbe
     merch_req_children = Merch_Requirement.objects.filter(requirement__in=req_children).filter(merchant=merchant).order_by(
         'merch_req_num_col1', 'merch_req_num_col2', 'merch_req_num_col3', 'merch_req_num_col4')
 
-    if request.method == 'POST':
-        if User in users_in_group:
+    if users_in_group :
+        if request.method == 'POST':
+
 
             # create a form instance and populate it with data from the request:
             form = NameForm(request.POST)
@@ -73,6 +75,8 @@ def merchant_requirement(request, merchant_name_URL, version, saq_req, req_numbe
 
                 # redirect to a new URL:
                 return HttpResponseRedirect('./')
+        else:
+            form = NameForm()
 
     # if a GET (or any other method) we'll create a blank form
     else:
