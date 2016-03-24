a456e34e5f9f276cdf1ec0e4d0f10a18ee5efa93
Jon Barclay <jon.barclay@gmail.com>
2016-03-24 09:48:40 -0600

    updated models and templates

--- models.py
+++ models.py
@@ -22,37 +22,37 @@ class Merchant(models.Model):
     req_complete = models.IntegerField(blank=True, null=True, default=0)
     total_req = models.IntegerField(blank=True, null=True, default=0)
     merchant_URL = models.URLField(max_length=2000, blank=True, null=True)
-    merchant_contact_name = models.CharField(max_length=200, blank=True, null=True)
-    merchant_contact_uvid = models.CharField(max_length=200, blank=True, null=True)
-    merchant_contact_phone = models.CharField(max_length=200, blank=True, null=True)
+    merchant_contact_name = models.CharField(max_length=100, blank=True, null=True)
+    merchant_contact_uvid = models.CharField(max_length=8, blank=True, null=True)
+    merchant_contact_phone = models.CharField(max_length=15, blank=True, null=True)
     merchant_contact_email = models.EmailField(blank=True, null=True)
-    merchant_business_description = models.TextField(blank=True, null=True)
-    merchant_card_processing_description = models.TextField(blank=True, null=True)
-#    merchant_payment_channels = models.TextField(blank=True, null=True)
-    merchant_third_parties = models.TextField(blank=True, null=True)
+    merchant_business_description = models.TextField(blank=True, null=True, help_text="Description of the merchant’s payment card business. This is not intended to be a cut-and-paste from the department’s website, but should be a tailored description that shows the assessor understands the business of the merchant being assessed.")
+    merchant_card_processing_description = models.TextField(blank=True, null=True, help_text="Describe how and why the merchant stores, processes, and/or transmits cardholder data. This is not intended to be a cut-and-paste from above, but should build on the understanding of the business and the impact this can have upon the security of cardholder data.")
+#    merchant_payment_channels = models.TextField(blank=True, null=True, help_text="What types of payment channels the merchant serves, such as card-present and card-not-present (for example, mail order/telephone order (MOTO), e-commerce).")
+    merchant_third_parties = models.TextField(blank=True, null=True, help_text="Any entities that the assessed merchant connects to for payment transmission or processing, including processor relationships.")
     # file will be saved to MEDIA_ROOT/uploads/2015/01/30
-    merchant_network_diagram = models.FileField(upload_to='network-diagram', blank=True, null=True)
-    merchant_process_identify_data = models.TextField(blank=True, null=True)
-    merchant_process_no_data_outside_cde = models.TextField(blank=True, null=True)
-    merchant_scope_review_process = models.TextField(blank=True, null=True)
-    merchant_scope_documentation = models.TextField(blank=True, null=True)
-    merchant_scope_reasoning = models.TextField(blank=True, null=True)
-    merchant_cde_people = models.TextField(blank=True, null=True)
-    merchant_cde_processes = models.TextField(blank=True, null=True)
-    merchant_cde_technologies = models.TextField(blank=True, null=True)
-    merchant_cde_locations = models.TextField(blank=True, null=True)
-    merchant_network_segmentation = models.TextField(blank=True, null=True)
-    merchant_cde_networks = models.TextField(blank=True, null=True)
-    merchant_in_scope_networks = models.TextField(blank=True, null=True)
-    merchant_out_of_scope_networks = models.TextField(blank=True, null=True)
-    merchant_processing_entities = models.TextField(blank=True, null=True)
-    merchant_wireless = models.TextField(blank=True, null=True)
-    merchant_data_flow_authorization = models.TextField(blank=True, null=True)
-    merchant_data_flow_capture = models.TextField(blank=True, null=True)
-    merchant_data_flow_settlement = models.TextField(blank=True, null=True)
-    merchant_data_flow_chargeback = models.TextField(blank=True, null=True)
-    merchant_card_storage = models.TextField(blank=True, null=True)
-    merchant_sampling = models.TextField(blank=True, null=True)
+    merchant_network_diagram = models.FileField(upload_to='network-diagram', blank=True, null=True, help_text="Provide a high-level network diagram (either obtained from the entity or created by assessor) of the entity’s networking topography, showing the overall architecture of the environment being assessed. This high-level diagram should summarize all locations and key systems, and the boundaries between them and should include the following: Connections into and out of the network including demarcation points between the cardholder data environment (CDE) and other networks/zones Critical components within the cardholder data environment, including POS devices, systems, databases, and web servers, as applicable Other necessary payment components, as applicable")
+    merchant_process_identify_data = models.TextField(blank=True, null=True, help_text="Describe the methods or processes (for example, tools, observations, feedback, scans, data flow analysis) used to identify and document all existences of cardholder data (as executed by the assessor, assessed entity or a combination):")
+    merchant_process_no_data_outside_cde = models.TextField(blank=True, null=True, help_text="Describe the methods or processes (for example, tools, observations, feedback, scans, data flow analysis) used to verify that no cardholder data exists outside of the defined CDE (as executed by the assessor, assessed entity or a combination):")
+    merchant_scope_review_process = models.TextField(blank=True, null=True, help_text="Describe how the results of the methods/processes were evaluated by the assessor to verify that the PCI DSS scope of review is appropriate:")
+    merchant_scope_documentation = models.TextField(blank=True, null=True, help_text="Describe how the results of the methods/processes were documented (for example, the results may be a diagram or an inventory of cardholder data locations):")
+    merchant_scope_reasoning = models.TextField(blank=True, null=True, help_text="Describe why the methods (for example, tools, observations, feedback, scans, data flow analysis) used for scope verification are considered by the assessor to be effective and accurate:")
+    merchant_cde_people = models.TextField(blank=True, null=True, help_text="People – such as technical support, management, administrators, operations teams, cashiers, telephone operators, etc.: Note – this is not intended to be a list of individuals interviewed, but instead a list of the types of people, teams, etc. who were included in the scope.")
+    merchant_cde_processes = models.TextField(blank=True, null=True, help_text="Processes – such as payment channels, business functions, etc.:")
+    merchant_cde_technologies = models.TextField(blank=True, null=True, help_text="Technologies – such as e-commerce systems, internal network segments, DMZ segments, processor connections, POS systems, etc.: Note – this is not intended to be a list of devices but instead a list of the types of technologies, purposes, functions, etc. included in the scope.")
+    merchant_cde_locations = models.TextField(blank=True, null=True, help_text="Locations/sites/stores – such as retail outlets, data centers, corporate office locations, call centers, etc.:")
+    merchant_network_segmentation = models.TextField(blank=True, null=True, help_text="Identify whether the assessed entity has used network segmentation to reduce the scope of the assessment. (yes/no) Explain how the assessor validated the effectiveness of the segmentation, as follows: Describe the methods used to validate the effectiveness of the segmentation (for example, observed configurations of implemented technologies, tools used, network traffic analysis, etc.). Describe how it was verified that the segmentation is functioning as intended. Describe how it was verified that adequate security controls are in place to ensure the integrity of the segmentation mechanisms (e.g., access controls, change management, logging, monitoring, etc.).")
+    merchant_cde_networks = models.TextField(blank=True, null=True, help_text="Describe all networks that store, process and/or transmit CHD:")
+    merchant_in_scope_networks = models.TextField(blank=True, null=True, help_text="Describe all networks that do not store, process and/or transmit CHD, but are still in scope (e.g., connected to the CDE or provide management functions to the CDE):")
+    merchant_out_of_scope_networks = models.TextField(blank=True, null=True, help_text="Describe any networks confirmed to be out of scope:")
+    merchant_processing_entities = models.TextField(blank=True, null=True, help_text="Identify All Processing Entities (Acquirer/ Bank/ Brands directly connected to for processing)")
+    merchant_wireless = models.TextField(blank=True, null=True, help_text="If there are no wireless networks or technologies in use, describe how this was verified by the assessor. If there are wireless networks or technologies in use, identify and describe all wireless technologies in use that are connected to or could impact the security of the cardholder data environment. This would include : Wireless LANs, Wireless payment applications (for example, POS terminals), All other wireless devices/technologies")
+    merchant_data_flow_authorization = models.TextField(blank=True, null=True, help_text="Describe how cardholder data is transmitted and/or processed and for what purpose it is used")
+    merchant_data_flow_capture = models.TextField(blank=True, null=True, help_text="Describe how cardholder data is transmitted and/or processed and for what purpose it is used")
+    merchant_data_flow_settlement = models.TextField(blank=True, null=True, help_text="Describe how cardholder data is transmitted and/or processed and for what purpose it is used")
+    merchant_data_flow_chargeback = models.TextField(blank=True, null=True, help_text="Describe how cardholder data is transmitted and/or processed and for what purpose it is used")
+    merchant_card_storage = models.TextField(blank=True, null=True, help_text="Identify and list all databases, tables, and files storing cardholder data and provide the following details. Note: The list of files and tables that store cardholder data in the table below must be supported by an inventory created (or obtained from the client) and retained by the assessor in the work papers.")
+    merchant_sampling = models.TextField(blank=True, null=True, help_text="Identify whether sampling was used during the assessment. Describe the sampling rationale and/or standardized PCI DSS security and operational processes/controls used for selecting sample sizes (for people, processes, technologies, devices, locations/sites, etc.).")
 
 #    saq_a = 'A'
 #    saq_aep = 'A-EP'
@@ -88,7 +88,7 @@ class Merchant(models.Model):
 
 
 class Hardware(models.Model):
-    hardware_device_type = models.CharField(max_length=200)
+    hardware_device_type = models.CharField(max_length=200, help_text="Identify and list all types of hardware in the cardholder environment, including network components, servers and other mainframes, devices performing security functions, end-user devices (such as laptops and workstations), virtualized devices (if applicable) and any other critical hardware – including homegrown components. For each item in the list, provide details for the hardware as indicated below. Add rows, as needed.")
     hardware_vendor = models.TextField(blank=True, null=True)
     hardware_role = models.TextField(blank=True, null=True)
 
@@ -101,7 +101,7 @@ class Hardware(models.Model):
 
 
 class Software(models.Model):
-    software_product = models.CharField(max_length=200)
+    software_product = models.CharField(max_length=200, help_text="Identify and list all critical software in the cardholder environment, such as e-commerce applications, applications accessing CHD for non-payment functions (fraud modeling, credit verification, etc.), software performing security functions or enforcing PCI DSS controls, underlying operating systems that store, process or transmit CHD, system management software, virtualization management software, and other critical software – including homegrown software/applications. For each item in the list, provide details for the software as indicated below. Add rows, as needed.")
     software_vendor = models.TextField(blank=True, null=True)
     software_role = models.TextField(blank=True, null=True)
 
@@ -114,10 +114,10 @@ class Software(models.Model):
 
 
 class ServiceProvider(models.Model):
-    service_provider_name = models.CharField(max_length=200)
-    service_provider_data_shared = models.TextField(blank=True, null=True)
-    service_provider_sharing_purpose = models.TextField(blank=True, null=True)
-    service_provider_pci_status = models.TextField(blank=True, null=True)
+    service_provider_name = models.CharField(max_length=200, help_text="Service providers and other third parties with which the entity shares cardholder data")
+    service_provider_data_shared = models.TextField(blank=True, null=True, help_text="What data is shared (for example, PAN, expiry date, etc.)")
+    service_provider_sharing_purpose = models.TextField(blank=True, null=True, help_text="The purpose for sharing the data (for example, third-party storage, transaction processing, etc.)")
+    service_provider_pci_status = models.TextField(blank=True, null=True, help_text="Status of PCI DSS Compliance (Date of AOC and version #)")
 
     merchants = models.ManyToManyField(Merchant)
 
@@ -126,7 +126,7 @@ class ServiceProvider(models.Model):
 
 
 class PaymentApplication(models.Model):
-    payment_application_name = models.CharField(max_length=200)
+    payment_application_name = models.CharField(max_length=200, help_text="List all third-party payment application products and version numbers in use, including whether each payment application has been validated according to PA-DSS or PCI P2PE. Even if a payment application has been PA-DSS or PCI P2PE validated, the assessor still needs to verify that the application has been implemented in a PCI DSS compliant manner and environment, and according to the payment application vendor’s PA-DSS Implementation Guide for PA-DSS applications or P2PE Implementation Manual (PIM) and P2PE application vendor’s P2PE Application Implementation Guide for PCI P2PE applications/solutions.")
     payment_application_vendor = models.CharField(max_length=200, blank=True, null=True)
     payment_application_description = models.TextField(blank=True, null=True)
     payment_application_version = models.TextField(blank=True, null=True)
